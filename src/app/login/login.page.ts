a2b2796145cb71c57d2ef45a58c899e22dfc0ae0
Moon3301 <carlos_db_16@live.cl>
2023-10-02 22:23:31 -0300

    commit

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  //modal
+  isModalOpenLogin = false;
+
+  isModalOpenRegistro = false;
+
+  // clave oculta
+  hide = true;
+
+  //
+
+  nombreUsuario:any
+  apellidoUsuario:any
+
+  correo:any
+  email = new FormControl('', [Validators.required, Validators.email]);
+  
+  contrasena:any
+  fotoUsuario:any
+  telefono:any
+  nombreNegocio:any
+  fotoNegocio:any
+
+
+  //
+
+
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  setOpenLogin(isOpen: boolean) {
+    this.isModalOpenLogin = isOpen;
+  }
+
+  setOpenRegistro(isOpen:boolean){
+    this.isModalOpenRegistro = isOpen;
+  }
+
+
+
+
 
 }
---
+++ src/app/negocio.ts
@@ -0,0 +1,6 @@
+export interface Negocio {
+
+    id:number
+    nombre:string
+    foto:string
+}
