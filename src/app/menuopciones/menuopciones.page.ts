9a9e09242b404c83d69e6f18d0edb7f8b3dfe22f
gabriellereyes <ga.reyesa@duocuc.cl>
2023-10-09 23:15:26 -0300

    coomit

--- src/app/menuopciones/menuopciones.page.ts
+++ src/app/menuopciones/menuopciones.page.ts
@@ -12,7 +12,10 @@ import { faGasPump, faCarOn, faSchool, faBuildingColumns, faCapsules, faShirt, f
   
 
 })
-//
+
+
+
+///
 export class MenuopcionesPage implements OnInit {
   faImage = faImage;
 
